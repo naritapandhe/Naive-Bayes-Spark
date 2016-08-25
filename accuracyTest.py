6069da5e8c3304e0ef1ce872a132944cf8dc4f22
Shubhi Jain <shubhi.j08@gmail.com>
2016-08-25 02:06:42 -0400

    Added code for finding % of correctly predicted labels

--- accuracyTest.py
+++ accuracyTest.py
@@ -27,52 +27,46 @@ def checkIfExist(labelList,output):
 		if "CAT" in word:
 			flag= True
 			if output == word.lower():
-            returnResults.append(('T',1))
-            break
+				returnResults.append(('T',1))
+				break
 			
-	if flag== True && len(returnResults)==0:
+	if flag== True and len(returnResults)==0:
 		returnResults.append(('F',1))
 		
-	if flag== False
+	if not flag:
 		returnResults.append(('S',1))
 	
 	return returnResults
 	
-	#<---original code--->
-	""returnResults = []
-    for word in labelList:
-        if output == word.lower():
-            returnResults.append(('T',1))
-            break
-        
-    if len(returnResults) == 0:
-        returnResults.append(('F',1))
-		
-	return returnResults""
-	#<----original code--->
-
     
     
 def main():
-     #read the labels from testing file
-     testData = read_document_data('https://s3.amazonaws.com/eds-uga-csci8360/data/project1/y_test_small.txt')
+	#read the labels from testing file
+	testData = read_document_data('https://s3.amazonaws.com/eds-uga-csci8360/data/project1/y_test_small.txt')
+
+	#create RDD of the testing labels
+	entireLabelData = sc.parallelize(testData).zipWithIndex().map(lambda doc:(doc[1],doc[0])).cache()
+	removeEndline = entireLabelData.map(lambda doc: (doc[0],doc[1].rstrip('\n')))
+	
      
-     entireLabelData = sc.parallelize(testData).zipWithIndex().map(lambda doc:(doc[1],doc[0])).cache()
-     removeEndline = entireLabelData.map(lambda doc: (doc[0],doc[1].rstrip('\n')))
+	#read the labels from output file
+	olist = sc.textFile("/DSP_Project1/alias-project1/oFile.txt")
+	olist1 = olist.map(lambda word:word.encode("utf-8"))
+	
+	#create RDD of the output labels
+	entireOtputData= olist1.zipWithIndex().map(lambda testDoc:(testDoc[1],testDoc[0])).cache()
 
-     
-     #read the labels from outut file
-     olist = sc.textFile("/Users/admin/Documents/pythonworkspace/data-science-practicum/project-1/alias-project1/oFile.txt")
-     olist1 = olist.map(lambda word:word.encode("utf-8"))
-     
-     entireOtputData= olist1.zipWithIndex().map(lambda testDoc:(testDoc[1],testDoc[0])).cache()
+	#==============================================================================
+	# Joining the documents and labels
+	#==============================================================================
+	
+	joinedRDD = removeEndline.join(entireOtputData)
+	labelsAndOutputJoinedRDD = joinedRDD.map(lambda (x,y):(x,y[0],y[1]))
+	
+	#This will return the no. of correct and incorrect predictions
 
-     joinedRDD = removeEndline.join(entireOtputData)
-     labelsAndOutputJoinedRDD = joinedRDD.map(lambda (x,y):(x,y[0],y[1]))
-
-
-     resulatantData= labelsAndOutputJoinedRDD.map(lambda doc:checkIfExist(doc[1].split(','),doc[2])).flatMap(lambda x:x).reduceByKey(lambda x,y:x+y)
-     print resulatantData.collect()
+	resulatantData= labelsAndOutputJoinedRDD.map(lambda doc:checkIfExist(doc[1].split(','),doc[2])).flatMap(lambda x:x).reduceByKey(lambda x,y:x+y)
+	print resulatantData.collect()
      
 
 if __name__ == "__main__":
