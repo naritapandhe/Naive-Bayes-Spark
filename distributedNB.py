a23e1056dfdead777580575707dc68684c13fff3
Narita Pandhe <narita.pandhe@gmail.com>
2016-08-30 01:19:26 -0400

    Added more stopwords, but no change in accuracy :(

--- distributedNB.py
+++ distributedNB.py
@@ -13,9 +13,9 @@ from collections import OrderedDict
 
 
 def read_stop_words():
-    #readStopWords = stopwords.words("english") 
-    readStopWords =  ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'yo', 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', 'her', 'hers', 'herself', 'it', 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', 'should', 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', 'couldn', 'didn', 'doesn', 'hadn', 'hasn', 'haven', 'isn', 'ma', 'mightn', 'mustn', 'needn', 'shan', 'shouldn', 'wasn', 'weren', 'won', 'wouldn','tel','fax','would','could','should']
-    #cachedStopWords = map(lambda word: word.encode("utf-8"),readStopWords)
+    with open("stopWordList.txt") as f:
+        readStopWords = [x.strip('\n').encode("utf-8") for x in f.readlines()]
+
     return readStopWords
 
 def read_data_from_url(url):
